bcc846cfd271eedc63aedff3e8e2ca5fd2f9df2c
Chen Jiacheng <3559084904@qq.com>
2024-11-16 18:12:34 +0000

    重构数据库

--- database/connectDB.ts
+++ database/connectDB.ts
@@ -1,7 +1,26 @@
-import { ProblemType } from "./problemDB"
-let problemSequence: ProblemType[]=[]
-type RequestType = {type : string, num : number}
-function getProblem(Request : RequestType[] | Request){
-  
+import { ProblemType, ProblemDB,ProblemClass } from "./problemDB"
+let database: ProblemDB;
+let problemCache: ProblemType[]=[]
+export type RequestType = {type : string, num : number}
+type OptionType = {optionId : number, content : string}
+async function getProblemFull(request : RequestType){
+  const seq = await database.randomFind(request.type,request.num);
+  return seq.map((value)=>{
+    let data : ProblemType = {
+      id : value.id,
+      problemType : value.problemType,
+      stem : value.stem??"",
+      options : value.options.sort((a:OptionType,b:OptionType)=>{
+	if (a.optionId>b.optionId)return 1;
+	if (a.optionId<b.optionId)return -1;
+	return 0;
+      }).map((value:OptionType)=>{return value.content})
+    }
+    return data
+  }
+  )
 }
-function
+export async function getProblem(request : RequestType){
+  const seq = await getProblemFull(request);
+
+}
--- database/problemDB.ts
+++ database/problemDB.ts
@@ -1,28 +1,30 @@
 import { PrismaClient } from "@prisma/client";
+import { isRight } from "./answerRule";
 const prisma = new PrismaClient();
 export type ProblemType= { //内部问题类型格式
-      type:string,
-      stem?:string,
-      options?: string[],
-      answers? : string,
-      answerRule? : string,
+  id :number,
+  problemType:string,
+  stem?:string,
+  options?: string[],
+  answers? : string,
+  answerRule? : string,
   //subProblem? :ProblemSeq
 }
 
 export class ProblemDB{
-  async create(problem:ProblemClass){ //创建一个问题
+  async create(problemType:string,stem:string,options:string[],answers:string,answerRule?:string){ //创建一个问题
     return (await prisma.problem.create(
       {
 	data: {
-	  stem : problem.element.stem,
-	  problemType : problem.element.type,
-	  answers: problem.element.answers??"",
-	  answerRule: problem.element.answerRule??"Unique",
+	  problemType : problemType,
+	  stem : stem,
+	  answers: answers,
+	  answerRule: answerRule??"Unique",
 	  options :{
-	    create : problem.element.options?.map(
+	    create : options.map(
 	      (value,index)=>{
 		return {optionId : index, content : value}
-	      })??undefined
+	      })
 	  }
 	    // ,
 	    //  subProblem :{
@@ -32,17 +34,57 @@ export class ProblemDB{
       }
     ));
   }
-  async randomFind(type : string ,length : number,domId? : number){ //随机选择特定类型的问题
-    return await prisma.problem.findMany({where : {problemType : type, domId: domId??undefined},orderBy : await prisma.$queryRaw `random()`,take : length})
+  async randomFindId(type : string ,length : number){ //随机选择特定类型的问题，返回ID
+    return (await prisma.problem.findMany(
+      {
+	select : {
+	  id : true
+	},
+	where : {problemType : type},
+	orderBy : await prisma.$queryRaw `random()`,
+	take : length
+      })).map((value)=>{return value.id})
   }
-  async list(type : string){
-    return await prisma.problem.findMany({where :{problemType : type}})
+  async problemFind(idList : number[]){ //返回ID列表的题目（不含答案）
+    return idList.map(async (id)=>{
+      return await prisma.problem.findUnique(
+	{
+	  select : {
+	    stem : true,
+	    problemType : true,
+	    options : {
+	      select : {
+		optionId : true,
+		content : true
+	      }
+	    }
+	  },
+	  where : {id : id}
+	}
+      )
+    })
   }
+  async answerCheck(checkList: {id : number, answer : string}){ // 检查特定ID题目的答案
+    return checkList.map(async (val:{id : number, answer : string})=>{
+      const ans=await prisma.problem.findUnique(
+	{
+	  select : {
+	    answers : true,
+	    answerRule : true
+	  },
+	  where : {id : val.id}
+	}
+      )
+      return isRight(val.answer,ans?.answers??"",ans?.answerRule??"")
+    })
+  }
+    /**
   async delete(id:number){ //删除特定id的问题
     //(await prisma.problem.findMany({where : {domId : id}})).forEach((val)=>this.delete(val.id))
     await prisma.problem.delete({where: {id : id}})
-  }
+    }*/
 }
+/**
 export class ProblemClass{
   element : ProblemType
   constructor(
@@ -54,9 +96,4 @@ export class ProblemClass{
   }
 
 }
-export class ProblemSeq{
-  element : ProblemClass[] = [];
-  constructor(element: ProblemClass[]){
-    this.element=element;
-  }
-}
+ */
